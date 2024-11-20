f3cf5450cdb7d9b1b8f679afae869914317ad8e1
Ram Narayan Jaiswal <145916274+RamNarayan3231@users.noreply.github.com>
2024-11-20 22:58:32 +0530

    Create Majority Element II.cpp

---
+++ Majority Element II.cpp
@@ -0,0 +1,37 @@
+/*
+      Day 03 
+      
+      #  Majority Element II  #
+
+      c++
+
+      20/11/2024
+
+*/
+
+
+vector<int> findMajority(vector<int>& arr) {
+        // Your code goes here.
+        sort(arr.begin(), arr.end());
+    int n = arr.size();
+    long long int co = 1;
+    vector<int> ans;
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            co++;
+        } else {
+            if (co > n / 3) {
+                ans.push_back(arr[i - 1]);
+            }
+            co = 1;
+        }
+    }
+    
+    // Check the last element
+    if (co > n / 3) {
+        ans.push_back(arr[n - 1]);
+    }
+    
+    return ans;
+    }
