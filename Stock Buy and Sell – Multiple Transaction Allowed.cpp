a131414a82a469171691a07e0487a6cc95988018
Ram Narayan Jaiswal <145916274+RamNarayan3231@users.noreply.github.com>
2024-11-21 23:08:35 +0530

    Create Stock Buy and Sell – Multiple Transaction Allowed.cpp

---
+++ Stock Buy and Sell – Multiple Transaction Allowed.cpp
@@ -0,0 +1,27 @@
+/*
+      Day 02 
+      
+      #  Stock Buy and Sell – Multiple Transaction Allowed  #
+
+      c++
+
+      21/11/2024
+
+*/
+
+int maximumProfit(vector<int> &prices) {
+        // code here
+        int n = prices.size();
+    if (n == 0) return 0;
+    
+    int profit = 0;
+    
+    for (int i = 1; i < n; i++) {
+        
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    
+    return profit;
+    }
